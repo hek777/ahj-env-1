40d58642e10232479a53aabc143689064514a447
Voinov Nikita <nikita-nikita2012@mail.ru>
2021-11-23 00:01:09 +0300

    commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,9 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-console */
 // TODO: write code here
 
 // comment this to pass build
+// eslint-disable-next-line no-unused-vars
 const unusedVariable = 'variable';
 
 // for demonstration purpose only
